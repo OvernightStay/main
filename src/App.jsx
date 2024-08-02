92bcacd628684fa43bcabe869eda30103205ef51
makedonzp <freefilosof@gmail.com>
2024-08-02 15:17:01 +0300

    front +

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import styles from "./App.module.css";
-import EnterForm from "./components/EnterForm/EnterForm";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { Greeting } from './components/Greeting';
+import { Authorization } from './components/Auth/Authorization';
+import { Registration } from './components/Auth/Registration';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <EnterForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/authorization" element={<Authorization />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
   );
 }
 
